7cc6e957c0731ecb9db850f8f4c87b685ba64c18
Jonathan Masmejean <azazel971@gmail.com>
2019-03-25 13:05:46 +0100

    Fix #16

--- src/dev/src/app/forms/forms-demo.module.ts
+++ src/dev/src/app/forms/forms-demo.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Type } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DefaultComponent } from './default/default.component';
 import { ROUTING } from './forms-demo.routing';
@@ -15,33 +15,28 @@ import { PasswordComponent } from './password/password.component';
 import { DefaultTemplateWrapper } from './default-template-wrapper';
 import { DefaultTemplateContent } from './default-template-content';
 import { DatetimeComponent } from './datetime/datetime.component';
+import { DemoComponent } from '../utils/demo.component';
+import { DemoPageComponent } from '../utils/demo-page.component';
+
+export const FUI_DEMO_FROMS_DIRECTIVES: Type<any>[] = [
+  DemoComponent,
+  DemoPageComponent,
+  DefaultTemplateWrapper,
+  DefaultTemplateContent,
+  DefaultComponent,
+  InputsComponent,
+  DatetimeComponent,
+  TextareaComponent,
+  CheckboxComponent,
+  RadiosComponent,
+  SelectsComponent,
+  PasswordComponent,
+  FormsLandingComponent,
+];
 
 @NgModule({
   imports: [CommonModule, FormsModule, FeruiModule, ROUTING, HighlightModule],
-  declarations: [
-    DefaultTemplateWrapper,
-    DefaultTemplateContent,
-    DefaultComponent,
-    InputsComponent,
-    DatetimeComponent,
-    TextareaComponent,
-    CheckboxComponent,
-    RadiosComponent,
-    SelectsComponent,
-    PasswordComponent,
-    FormsLandingComponent,
-  ],
-  exports: [
-    DefaultTemplateWrapper,
-    DefaultTemplateContent,
-    DefaultComponent,
-    InputsComponent,
-    DatetimeComponent,
-    TextareaComponent,
-    CheckboxComponent,
-    RadiosComponent,
-    SelectsComponent,
-    PasswordComponent,
-  ],
+  declarations: [FUI_DEMO_FROMS_DIRECTIVES],
+  exports: [FUI_DEMO_FROMS_DIRECTIVES],
 })
 export class FormsDemoModule {}
